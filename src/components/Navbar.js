aecda247d56f975f62ec663a5d034265e6c305f5
iamaashk <adnanahmadshaikh2003@gmail.com>
2024-03-23 03:11:14 +0530

    v1.0.0.0.2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,19 @@ const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-* ">
     <div class="container-fluid ml-auto">
-      <a class="navbar-brand" href="#">Portofolio</a>
+      
+      <div className='port'>Portfolio</div>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link" href="#">Introduction</a>
-          <a class="nav-link" href="#">Work</a>
-          <a class="nav-link" href="#">Acheivements</a>
-          <a class="nav-link" href='#'>Education</a>
-          <a class="nav-link" href='#'>Contact</a>
+        <button className='btn'>Introduction</button>
+        <button className='btn'>Work</button>
+        <button className='btn'>Education</button>
+        <button className='btn'>Acheivements</button>
+        <button className='btn'>Contact</button>
+         
         </div>
       </div>
     </div>
